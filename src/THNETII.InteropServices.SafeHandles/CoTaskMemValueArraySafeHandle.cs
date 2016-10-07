7d38555b578aa8c30ce06af446c1d2076621d422
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2016-10-07 10:59:26 +0200

    Initial CoTaskMem Value Array Safe Handle

---
+++ src/THNETII.InteropServices.SafeHandles/CoTaskMemValueArraySafeHandle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace THNETII.InteropServices.SafeHandles
+{
+    public class CoTaskMemValueArraySafeHandle<T> : ValueArraySafeHandle<T>
+    {
+        public override bool IsInvalid => handle == IntPtr.Zero;
+
+        protected override bool ReleaseHandle()
+        {
+            var currentHandle = Interlocked.Exchange(ref handle, IntPtr.Zero);
+            if (currentHandle == IntPtr.Zero)
+                return false;
+            Marshal.FreeCoTaskMem(handle);
+            return true;
+        }
+
+        protected CoTaskMemValueArraySafeHandle(IntPtr invalidHandleValue, bool ownsHandle) : base(invalidHandleValue, ownsHandle) { }
+    }
+}
---
+++ src/THNETII.InteropServices.SafeHandles/ValueArraySafeHandle.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace THNETII.InteropServices.SafeHandles
+{
+    public abstract class ValueArraySafeHandle<T> : SafeHandle<T[]>
+    {
+        public ValueArraySafeHandle(IntPtr invalidHandleValue, bool ownsHandle) : base(invalidHandleValue, ownsHandle) { }
+    }
+}
