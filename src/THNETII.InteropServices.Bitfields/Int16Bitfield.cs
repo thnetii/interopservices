c5bff204d5632f465ea11cfbbd37bd7d0a0a071f
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2016-10-07 10:58:37 +0200

    Short/UShort Bitfields

---
+++ src/THNETII.InteropServices.Bitfields/Int16Bitfield.cs
@@ -0,0 +1,23 @@
+﻿namespace THNETII.InteropServices.Bitfields
+{
+    public class Int16Bitfield : BitfieldBase, IBitfield<short>
+    {
+        private const int typeWidth = sizeof(short) * 8;
+        private const uint typeMask = 0xFFFF;
+
+        public override int TypeWidth => typeWidth;
+
+        public override uint TypeMask => typeMask;
+
+        public Int16Bitfield(int fieldWidth) : base(fieldWidth) { }
+
+        public short Get(short storage) => (short)(storage & fieldMask);
+
+        public short Set(short field, short storage) => (short)(((ushort)field & fieldMask) | ((ushort)storage & notMask));
+
+        public void RefSet(short field, ref short storage)
+        {
+            storage = (short)(((ushort)field & fieldMask) | ((ushort)storage & notMask));
+        }
+    }
+}
---
+++ src/THNETII.InteropServices.Bitfields/UInt16Bitfield.cs
@@ -0,0 +1,23 @@
+﻿namespace THNETII.InteropServices.Bitfields
+{
+    public class UInt16Bitfield : BitfieldBase, IBitfield<ushort>
+    {
+        private const int typeWidth = sizeof(ushort) * 8;
+        private const uint typeMask = 0xFFFF;
+
+        public override int TypeWidth => typeWidth;
+
+        public override uint TypeMask => typeMask;
+
+        public UInt16Bitfield(int fieldWidth) : base(fieldWidth) { }
+
+        public ushort Get(ushort storage) => (ushort)(storage & fieldMask);
+
+        public ushort Set(ushort field, ushort storage) => (ushort)((field & fieldMask) | (storage & notMask));
+
+        public void RefSet(ushort field, ref ushort storage)
+        {
+            storage = (ushort)((field & fieldMask) | (storage & notMask));
+        }
+    }
+}
