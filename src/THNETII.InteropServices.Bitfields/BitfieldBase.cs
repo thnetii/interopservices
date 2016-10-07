06d06e81d4304aac322b2ca5b7dd113376816e6a
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2016-10-07 10:56:52 +0200

    Basic Bitfield classes

---
+++ src/THNETII.InteropServices.Bitfields/BitfieldBase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace THNETII.InteropServices.Bitfields
+{
+    public abstract class BitfieldBase : IBitfield
+    {
+        protected readonly int fieldWidth;
+        protected readonly uint fieldMask;
+        protected readonly uint notMask;
+
+        public BitfieldBase(int fieldWidth)
+        {
+            if (fieldWidth < 1 || fieldWidth > 7)
+                throw new ArgumentOutOfRangeException(paramName: nameof(fieldWidth), actualValue: fieldWidth, message: $"The width of a bitfield with a {TypeWidth}-bit storage type must be a value between 0 and {TypeWidth}.");
+
+            this.fieldWidth = fieldWidth;
+            fieldMask = 0;
+            for (int i = 0; i < fieldWidth; i++)
+                fieldMask |= (1U << i);
+            notMask = TypeMask & ~fieldMask;
+        }
+
+        public abstract int TypeWidth { get; }
+
+        public abstract uint TypeMask { get; }
+
+        public virtual int FieldWidth => fieldWidth;
+
+        public virtual uint FieldMask => fieldMask;
+
+        public virtual uint InvertedMask => notMask;
+    }
+}
---
+++ src/THNETII.InteropServices.Bitfields/IBitfield.cs
@@ -0,0 +1,20 @@
+﻿namespace THNETII.InteropServices.Bitfields
+{
+    public interface IBitfield
+    {
+        int TypeWidth { get; }
+        uint TypeMask { get; }
+        int FieldWidth { get; }
+        uint FieldMask { get; }
+        uint InvertedMask { get; }
+    }
+
+    public interface IBitfield<T> : IBitfield
+    {
+        T Get(T storage);
+
+        T Set(T field, T storage);
+
+        void RefSet(T field, ref T storage);
+    }
+}
