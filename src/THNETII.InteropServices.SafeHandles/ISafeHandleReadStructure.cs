289d67cea3a5ca7dfa53c8457c13e467be99b2af
Fredrik Høisæther Rasch <fredrik.h.rasch@uit.no>
2017-03-02 19:31:48 +0100

    SafeHandle for default marshable structures

---
+++ src/THNETII.InteropServices.SafeHandles/ISafeHandleReadStructure.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace THNETII.InteropServices.SafeHandles
+{
+    public interface ISafeHandleReadStructure<out T> where T : new()
+    {
+    }
+}
---
+++ src/THNETII.InteropServices.SafeHandles/StructureSafeHandleExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace THNETII.InteropServices.SafeHandles
+{
+    public static class StructureSafeHandleExtensions
+    {
+        internal static T ReadStructure<T>(this SafeHandle safeHandle)
+            where T : new()
+        {
+            if (safeHandle == null)
+                throw new ArgumentNullException(nameof(safeHandle));
+            else if (safeHandle.IsClosed)
+                throw new ObjectDisposedException(nameof(safeHandle));
+            else if (safeHandle.IsInvalid)
+                throw new InvalidOperationException("The specified safe handle is an invalid handle.");
+            bool needsSafeHandleRelease = false;
+            safeHandle.DangerousAddRef(ref needsSafeHandleRelease);
+            try
+            {
+                return Marshal.PtrToStructure<T>(safeHandle.DangerousGetHandle());
+            }
+            finally { safeHandle.DangerousRelease(); }
+        }
+
+        public static T ReadValue<THandle, T>(this THandle safeHandle)
+            where T : new()
+            where THandle : SafeHandle, ISafeHandleReadStructure<T>
+            => safeHandle.ReadStructure<T>();
+    }
+}
