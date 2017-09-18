9c58bc77a66fa8fef5502935a4eec440aea82639
Fredrik Høisæther Rasch <fredrik.h.rasch@uit.no>
2017-09-18 16:06:58 +0200

    Completed basic custom marshaling tests

--- test/THNETII.InteropServices.Test/StructMarshal.Test/ICustomMarshalerBasicTest.cs
+++ test/THNETII.InteropServices.Test/StructMarshal.Test/ICustomMarshalerBasicTest.cs
@@ -29,8 +29,56 @@ namespace THNETII.InteropServices.StructMarshal.Test
         [Fact]
         public void ProxyMarshalerGetNativeDataSizeReturnsNegativeOne()
         {
-            var marshaler = ProxyMarshaler<DummyStruct>.GetInstance();
+            ICustomMarshaler marshaler = ProxyMarshaler<DummyStruct>.GetInstance();
             Assert.Equal(-1, marshaler.GetNativeDataSize());
+        }
+
+        [Fact]
+        public void ProxyMarshalerZeroPointerMarshalsToNull()
+        {
+            var marshaler = ProxyMarshaler<DummyStruct>.GetInstance();
+            Assert.Null(marshaler.MarshalNativeToManaged(IntPtr.Zero));
+        }
+
+        [Fact]
+        public void ProxyMarshalerNullMarshalsToZeroPointer()
+        {
+            var marshaler = ProxyMarshaler<DummyStruct>.GetInstance();
+            Assert.Equal(IntPtr.Zero, marshaler.MarshalManagedToNative(null));
+        }
+
+        [Fact]
+        public void ProxyMarshalerMarshalsDummyStructToNative()
+        {
+            const int expectedFieldValue = 42;
+            var marshaler = ProxyMarshaler<DummyStruct>.GetInstance();
+            var instance = new DummyStruct { field = expectedFieldValue };
+            var ptr = marshaler.MarshalManagedToNative(instance);
+            Assert.NotEqual(IntPtr.Zero, ptr);
+            try
+            {
+                //  field in DummyStruct is at offset 0 (it's the first field)
+                var fieldValue = Marshal.ReadInt32(ptr, ofs: 0);
+                Assert.Equal(expectedFieldValue, fieldValue);
+            }
+            finally { marshaler.CleanUpNativeData(ptr); }
+        }
+
+        [Fact]
+        public void ProxyMarshalerMarshalsDummyStructFromNative()
+        {
+            const int expectedFieldValue = 42;
+            var ptr = Marshal.AllocCoTaskMem(Marshal.SizeOf<DummyStruct>());
+            try
+            {
+                //  field in DummyStruct is at offset 0 (it's the first field)
+                Marshal.WriteInt32(ptr, ofs: 0, val: expectedFieldValue);
+                var marshaler = ProxyMarshaler<DummyStruct>.GetInstance();
+                var instance = marshaler.MarshalNativeToManaged(ptr) as DummyStruct;
+                Assert.NotNull(instance);
+                Assert.Equal(expectedFieldValue, instance.field);
+            }
+            finally { Marshal.FreeCoTaskMem(ptr); }
         }
     }
 }
