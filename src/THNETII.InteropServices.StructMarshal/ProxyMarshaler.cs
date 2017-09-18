66ce3af4d103d47e2ad7e86ab39e4851e0147c18
Fredrik Høisæther Rasch <fredrik.h.rasch@uit.no>
2017-09-18 10:03:11 +0200

    Started on ProxyMarshaler

---
+++ src/THNETII.InteropServices.StructMarshal/ProxyMarshaler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace THNETII.InteropServices.StructMarshal
+{
+    public class ProxyMarshaler<T> : ICustomMarshaler where T : new()
+    {
+        static ProxyMarshaler()
+        {
+
+        }
+
+        /// <inheritdoc />
+        public void CleanUpManagedData(object ManagedObj)
+        {
+            if (ManagedObj is IDisposable disposable)
+                disposable.Dispose();
+        }
+
+        public void CleanUpNativeData(IntPtr pNativeData)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public int GetNativeDataSize() => -1;
+
+        public IntPtr MarshalManagedToNative(object ManagedObj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object MarshalNativeToManaged(IntPtr pNativeData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static ICustomMarshaler GetInstance(string cookie = null) => new ProxyMarshaler<T>();
+    }
+}
---
+++ test/THNETII.InteropServices.Test/StructMarshal.Test/ICustomMarshalerBasicTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.InteropServices;
+using Xunit;
+
+namespace THNETII.InteropServices.StructMarshal.Test
+{
+    public class ICustomMarshalerBasicTest
+    {
+        [StructLayout(LayoutKind.Sequential)]
+        class DummyStruct { public int field; }
+
+        [Fact]
+        public void ProxyMarshalerGetInstanceReturnsCustomMarshaler()
+        {
+            var marshaler = ProxyMarshaler<DummyStruct>.GetInstance(null);
+            Assert.IsType<ProxyMarshaler<DummyStruct>>(marshaler);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("test")]
+        public void ProxyMarshalerGetInstanceTakesAnyStringValue(string cookie)
+        {
+            var marshaler = ProxyMarshaler<DummyStruct>.GetInstance(cookie);
+        }
+
+        [Fact]
+        public void ProxyMarshalerGetNativeDataSizeReturnsNegativeOne()
+        {
+            var marshaler = ProxyMarshaler<DummyStruct>.GetInstance();
+            Assert.Equal(-1, marshaler.GetNativeDataSize());
+        }
+    }
+}
