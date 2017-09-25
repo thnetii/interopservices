dd9dec84d95f83faee86a9f33a97cc7ff0290886
Fredrik Høisæther Rasch <fredrik.h.rasch@uit.no>
2017-09-25 16:40:08 +0200

    Field Offset and order marshal attributes

---
+++ src/THNETII.InteropServices.StructMarshal/MarshalFieldOrderAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace THNETII.InteropServices.StructMarshal
+{
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, Inherited = true, AllowMultiple = false)]
+    public class MarshalFieldOrderAttribute : Attribute
+    {
+        public MarshalFieldOrderAttribute(int order)
+        {
+            if (order < 0)
+                throw new ArgumentOutOfRangeException(nameof(order), order, "Value must not be negative.");
+            Value = order;
+        }
+
+        public int Value { get; }
+    }
+}
