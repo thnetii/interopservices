95f6f1095ec594a578549bf6da89070662de9f4b
Fredrik Høisæther Rasch <fredrik.h.rasch@uit.no>
2017-09-25 16:39:50 +0200

    Added simple test cases for Marshal Layout attribute

---
+++ test/THNETII.InteropServices.Test/StructMarshal.Test/MarshalLayoutAttributeTest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.InteropServices;
+using Xunit;
+
+namespace THNETII.InteropServices.StructMarshal.Test
+{
+    public class MarshalLayoutAttributeTest
+    {
+        [Fact]
+        public void CanConvertToStructLayoutAttributeInstance()
+        {
+            var mla = new MarshalLayoutAttribute(LayoutKind.Sequential)
+            {
+                CharSet = CharSet.Unicode,
+                Pack = 32,
+                Size = 128
+            };
+
+            var sla = (StructLayoutAttribute)mla;
+            Assert.NotNull(sla);
+            Assert.Equal(mla.LayoutKind, sla.Value);
+            Assert.Equal(mla.CharSet, sla.CharSet);
+            Assert.Equal(mla.Pack, sla.Pack);
+            Assert.Equal(mla.Size, sla.Size);
+        }
+
+        [Fact]
+        public void ConvertsNullMarshalLayoutAttributeToNull()
+        {
+            MarshalLayoutAttribute mla = null;
+            var sla = (StructLayoutAttribute)mla;
+            Assert.Null(sla);
+        }
+
+        [Fact]
+        public void CanConvertFromStructLayoutAttributeInstance()
+        {
+            var sla = new StructLayoutAttribute(LayoutKind.Sequential)
+            {
+                CharSet = CharSet.Unicode,
+                Pack = 32,
+                Size = 128
+            };
+
+            var mla = (MarshalLayoutAttribute)sla;
+            Assert.NotNull(mla);
+            Assert.Equal(sla.Value, mla.LayoutKind);
+            Assert.Equal(sla.CharSet, mla.CharSet);
+            Assert.Equal(sla.Pack, mla.Pack);
+            Assert.Equal(sla.Size, mla.Size);
+        }
+
+        [Fact]
+        public void ConvertsNullStructLayoutAttributeToNull()
+        {
+            StructLayoutAttribute sla = null;
+            var mla = (MarshalLayoutAttribute)sla;
+            Assert.Null(mla);
+        }
+    }
+}
