a451fb2b1102dff222139cd1902613ec4e0f6693
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2018-08-28 10:23:06 +0200

    Stub: Pointer to zero terminated unicode string

---
+++ src/THNETII.InteropServices.NativeMemory/UntypedPointer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+
+namespace THNETII.InteropServices.NativeMemory
+{
+    [StructLayout(LayoutKind.Sequential)]
+    [SuppressMessage("Style", "IDE0044:Add readonly modifier")]
+    public class UntypedPointer
+    {
+        private IntPtr ptr;
+
+        public IntPtr Pointer => ptr;
+    }
+}
