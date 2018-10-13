0c5dc348224190bcf328896bb4e89473899511a8
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2018-10-13 17:32:20 +0200

    Add wide character string pointer type

---
+++ src/THNETII.InteropServices.NativeMemory/Specialized/WideStringPtr.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+
+namespace THNETII.InteropServices.NativeMemory.Specialized
+{
+    /// <summary>
+    /// A pointer to a seqence of wide UTF-16 characters representing a
+    /// UTF-16 native string.
+    /// </summary>
+    /// <remarks>
+    /// Pointer to wide-characters sequences are also known in C APIs as:
+    /// <list type="bullet">
+    /// <item><term><c>wchar_t*</c></term></item>
+    /// <item><term><c>PWSTR</c></term></item>
+    /// <item><term><c>LPWSTR</c></term></item>
+    /// </list>
+    /// </remarks>
+    [StructLayout(LayoutKind.Sequential)]
+    [SuppressMessage("Performance", "CA1815: Override equals and operator equals on value types", Justification = nameof(IntPtrOfTExtensions))]
+    public struct WideStringPtr : IArrayPtr<char>
+    {
+        /// <summary>
+        /// Initializes a new typed pointer with the specified pointer to an unspecified type.
+        /// </summary>
+        /// <param name="ptr">A pointer to an unspecified type.</param>
+        public WideStringPtr(IntPtr ptr) => Pointer = ptr;
+
+        /// <inheritdoc />
+        public IntPtr Pointer { get; }
+    }
+}
---
+++ src/THNETII.InteropServices.NativeMemory/WideStringPtrExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System.Runtime.InteropServices;
+
+namespace THNETII.InteropServices.NativeMemory
+{
+    /// <summary>
+    /// Provides extension methods for Wide character UTF-16 string pointers.
+    /// </summary>
+    public static class WideStringPtrExtensions
+    {
+        /// <summary>
+        /// Allocated a new managed <see cref="string"/> instance and copies
+        /// the charaters from the pointer to the new <see cref="string"/> until
+        /// the first null-character is found.
+        /// </summary>
+        /// <param name="stringPtr">The pointer to marshal.</param>
+        /// <returns>
+        /// A managed string that holds a copy of the unmanaged string if the value of the
+        /// <paramref name="stringPtr"/> parameter is not <c>null</c> or a null-pointer;<br/>
+        /// otherwise, this method returns <c>null</c>.
+        /// </returns>
+        /// <seealso cref="Marshal.PtrToStringUni(System.IntPtr)"/>
+        public static string MarshalAsString(this IArrayPtr<char> stringPtr) =>
+            Marshal.PtrToStringUni((stringPtr?.Pointer).GetValueOrDefault());
+
+        /// <summary>
+        /// Allocated a new manages <see cref="string"/> instance and copies
+        /// the specified number of charaters from the pointer to the new <see cref="string"/>.
+        /// </summary>
+        /// <param name="stringPtr">The pointer to marshal.</param>
+        /// <param name="length">The number of Unicode characters to copy.</param>
+        /// <returns>
+        /// A managed string that holds a copy of the unmanaged string if the value of the
+        /// <paramref name="stringPtr"/> parameter is not <c>null</c> or a null-pointer;<br/>
+        /// otherwise, this method returns <c>null</c>.
+        /// </returns>
+        /// <seealso cref="Marshal.PtrToStringUni(System.IntPtr, int)"/>
+        public static string MarshalAsString(this IArrayPtr<char> stringPtr, int length) =>
+            Marshal.PtrToStringUni((stringPtr?.Pointer).GetValueOrDefault(), length);
+    }
+}
