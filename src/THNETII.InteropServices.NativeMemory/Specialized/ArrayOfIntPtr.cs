54b19aeeafa9e49baaf299cc4f049400da36e384
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2018-10-13 17:31:59 +0200

    Remove superfluous ToString method overrides

--- src/THNETII.InteropServices.NativeMemory/Specialized/ArrayOfIntPtr.cs
+++ src/THNETII.InteropServices.NativeMemory/Specialized/ArrayOfIntPtr.cs
@@ -19,20 +19,5 @@ namespace THNETII.InteropServices.NativeMemory.Specialized
 
         /// <inheritdoc />
         public IntPtr Pointer { get; }
-
-        /// <summary>
-        /// Converts the numeric value of the current pointer to its equivalent
-        /// string representation.
-        /// </summary>
-        /// <returns>The string representation of the value of this instance.</returns>
-        public override string ToString() => Pointer.ToString();
-
-        /// <summary>
-        /// Converts the numeric value of the current pointer to its equivalent
-        /// string representation.
-        /// </summary>
-        /// <param name="format">A format specification that governs how the current pointer is converted.</param>
-        /// <returns>The string representation of the value of this instance.</returns>
-        public string ToString(string format) => Pointer.ToString(format);
     }
 }
